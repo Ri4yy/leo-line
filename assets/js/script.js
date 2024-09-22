2160fab1c96c6c346cc5d6ac11372f8a6c6cbe64
ri4y <maksimov.1913@list.ru>
2024-09-22 21:08:46 +0300

    upd add new page

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,6 +74,35 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    let filterMainOpen = document.querySelector('.filter-btn-more'),
+        filterMainText = filterMainOpen.querySelector('.filter-input__name'),
+        filterOther = document.querySelector('.hero__wrapper');
+
+    let toggleFilter = true;
+
+    filterMainOpen.addEventListener('click', (e) => {
+        filterOther.classList.toggle('open')
+
+        filterMainText.textContent = toggleFilter ? 'Скрыть' : 'Еще фильтры';
+        toggleFilter = !toggleFilter;
+    })
+
+
+    // Фильтр в каталоге
+    let filter = document.querySelector('.catalog__side'),
+        filterOpen = document.querySelector('.catalog__menu'),
+        filterClose = document.querySelector('.filter__close');
+    
+    if(filter) {
+        filterOpen.addEventListener('click', (e) => {
+            filter.classList.add('open')
+        })
+
+        filterClose.addEventListener('click', (e) => {
+            filter.classList.remove('open')
+        })
+    }
+
 
     // Аккордеон
     $('.accordion__top').click((e) => {
@@ -81,4 +110,64 @@ document.addEventListener('DOMContentLoaded', () => {
         container.find('.accordion__bottom').toggleClass('open')
         container.find('.accordion__top').toggleClass('open')
     })
+    $('.filter-category__top').click((e) => {
+        let container = $(e.target).closest('.filter-category__list');
+        container.toggleClass('open')
+    })
+
+    // Модальное окно
+    function showModal(btnOpen, modalBody) {
+        btnOpen.click(function () {
+            modalBody.addClass('active');
+            $('html').addClass('no-scroll');
+            return false;
+        });
+
+        $(document).keydown(function (e) {
+            if (e.keyCode === 27) {
+                e.stopPropagation();
+                modalBody.removeClass('active');
+                $('html').removeClass('no-scroll');
+            }
+        });
+
+        modalBody.click(function (e) {
+            if ($(e.target).closest('.modal__wrapper').length == 0) {
+                $(this).removeClass('active');
+                $('html').removeClass('no-scroll');
+            }
+        });
+
+        $('.close-modal').click((e) => {
+            modalBody.removeClass('active');
+            $('html').removeClass('no-scroll');
+        })
+    }
+
+    showModal($('.feedback'), $('.modal--form'));
+    showModal($('.mobile-search'), $('.modal--search'));
+    showModal($('.mobile-contacts'), $('.modal--contacts'));
+
+    const listItems = document.querySelectorAll('.card-table--char .card-table__item');
+    const moreButton = document.querySelector('.card-table__more');
+    const visibleItemsCount = 6;
+    let isExpanded = false;
+    
+    if(listItems.length) {
+        function updateListVisibility() {
+            listItems.forEach((item, index) => {
+                if (index >= visibleItemsCount) {
+                    item.style.display = isExpanded ? 'flex' : 'none';
+                }
+            });
+            moreButton.textContent = isExpanded ? 'Скрыть' : 'Показать еще';
+        }
+    
+        updateListVisibility();
+    
+        moreButton.addEventListener('click', function() {
+            isExpanded = !isExpanded;
+            updateListVisibility();
+        });
+    }
 })
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,72 +1,71 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    spaceBetween: 20,
+document.addEventListener('DOMContentLoaded', () => {
+    let swiperThumbs = new Swiper(".swiper-thumbs", {
+        spaceBetween: 10,
+        slidesPerView: 5,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    let swiperMain = new Swiper(".swiper-main", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-main__next",
+            prevEl: ".swiper-main__prev",
+        },
+        pagination: {
+            el: '.swiper-main__pagination',
+        },
+        thumbs: {
+            swiper: swiperThumbs,
+        },
+    });
+
+    let swiperSimilar = new Swiper('.swiper-similar', {
+    spaceBetween: 40,
     enabled: true,
 
     breakpoints: {
         320: {
             allowTouchMove: true,
-            slidesPerView: 1,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
+            slidesPerView: 1 
+        },
+        375: {
+            allowTouchMove: true,
+            slidesPerView: 2,
+            spaceBetween: 7
         },
         480: {
             allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
+            slidesPerView: 2.1,
+            spaceBetween: 10
         },
         768: {
             allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
+            slidesPerView: 2.5,
+            spaceBetween: 20
         },
         1024: {
             allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
+            slidesPerView: 3
         },
         1280: {
             allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
+            slidesPerView: 4
         },
         1921: {
             allowTouchMove: true,
-            slidesPerView: 4,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
+            slidesPerView: 5
         },
     },
-  
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-similar__pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+        nextEl: '.swiper-similar__next',
+        prevEl: '.swiper-similar__prev',
     },
 });
+})
